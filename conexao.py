6aaaab3180e2e62c75b63cfe62eb6f1eb0cda366
Thais Minas <thaisminasds@gmail.com>
2021-05-13 22:25:19 -0300

    adicionando metodos da class cadastro

--- conexao.py
+++ conexao.py
@@ -4,11 +4,11 @@ conn = sqlite3.connect('cadastro.db')
 cursor = conn.cursor()
 
 
-class Banco:
+class Cadastro:
     def __init__(self):
         pass
 
-    def inserir(self, anuncio, cliente, data_inicio, data_termino, investimento):
+    def insert(self, anuncio, cliente, data_inicio, data_termino, investimento):
         cursor.execute("""
         INSERT INTO cadastro (anuncio, cliente, data_inicio, data_termino, investimento)
         VALUES ('{}', '{}', '{}', '{}', '{}')
@@ -17,14 +17,53 @@ class Banco:
         print('Dados inseridos com sucesso')
         conn.close()
 
-    def update(self):
-        pass
+    def consult(self):
+        try:
+            campo = input('Insira o registro: ')
+            consulta = ('SELECT * FROM cadastro WHERE id = {}'.format(campo))
+            cursor.execute(consulta)
+            conn.commit()
+            linhas = cursor.fetchone()
 
-    def delete(self):
-        pass
+            for linha in linhas:
+                print(linha)
+
+        except Exception as erro:
+            print('Falha ao consultar tabela: {}'. format(erro))
+        finally:
+            conn.close()
+
+    def deleteById(self):
+        try:
+            campo = input('Insira o registro para deletar: ')
+            apagar = ('DELETE FROM cadastro WHERE id = {}'.format(campo))
+            cursor.execute(apagar)
+            conn.commit()
+
+        except Exception as erro:
+            print('Falha ao deletar campo: {}'.format(erro))
+        finally:
+            conn.close()
+
+    def deleteByName(self):
+        try:
+            campo = input('Insira o registro para deletar: ')
+            apagar = ('DELETE FROM cadastro WHERE cliente = {}'.format(campo))
+            cursor.execute(apagar)
+            conn.commit()
+
+        except Exception as erro:
+            print('Falha ao deletar campo: {}'.format(erro))
+        finally:
+            conn.close()
+
 
     def get(self):
         pass
+
+    def update(self):
+        pass
+
 
 
 
